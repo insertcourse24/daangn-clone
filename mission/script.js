bf0cc2b9c0af0b9f277894c35ec35238612ff0c5
gamzamandu <galbimandu1126@gmail.com>
2024-07-06 20:43:11 +0900

    feat: acticle JSON 데이터 적용

--- mission/script.js
+++ mission/script.js
@@ -4,3 +4,23 @@
 // 2. 살포하기
 
 
+const url = '/data.json';
+
+const div = document.querySelector('#card-wrap');
+
+async function fetchJsonFile() {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    data.data.map((dataa)=>{
+        div.innerHTML += `<card-comp 
+        title="${dataa.name}"
+        price="${dataa.price}원"
+        region-name="${dataa.address}"
+        imgSrc="${dataa.img}">
+        </card-comp>`;
+        console.log(dataa);
+    });
+}
+
+fetchJsonFile();
