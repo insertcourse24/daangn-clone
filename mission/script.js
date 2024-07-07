36ce8e77542358b136a7f90de49294c548744859
gamzamandu <galbimandu1126@gmail.com>
2024-07-07 18:08:45 +0900

    chore : footer-top 제작

--- mission/script.js
+++ mission/script.js
@@ -1,9 +1,6 @@
 // 화이팅..
 
-// 1. JSON 리스트에 담기
-// 2. 살포하기
-
-
+//JSON 살포 코드
 const url = '/data.json';
 
 const div = document.querySelector('#card-wrap');
@@ -19,15 +16,38 @@ async function fetchJsonFile() {
         region-name="${dataa.address}"
         imgSrc="${dataa.img}">
         </card-comp>`;
-        console.log(dataa);
+        // console.log(dataa);
     });
 }
 
 fetchJsonFile();
 
+//keywords 살포 코드
 const keywordList = document.querySelector('#keywords-list');
 const keywords = ['알바','자전거','나눔','최강야구','한화','프리퀀시','에어컨','선풍기','제습기','삼성라이온즈'];
 
 keywords.forEach(element => {
     keywordList.innerHTML += `<keyword-comp text="${element}"></keyword-comp>`;
+});
+
+
+//footer INFO살포 코드
+const footerTopKeyword = document.getElementById('footer-top-keyword');
+const footerKeywordList = [
+    ["중고거래", "동네업체", "당근알바", "부동산", "중고차 직거래"],
+    ["당근비지니스", "채팅하기"],
+    ["자주 묻는 질문", "회사 소개", "인재 채용"]
+];
+
+
+footerKeywordList.forEach(category => {
+    const ul = document.createElement('ul');
+
+    category.forEach(item => {
+        const keywordComp = document.createElement('keyword-comp');
+        keywordComp.setAttribute('text', item);
+        ul.appendChild(keywordComp);
+    });
+
+    footerTopKeyword.appendChild(ul);
 });
