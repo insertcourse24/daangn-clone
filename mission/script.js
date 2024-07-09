7692a8a9dbfb45616de587265bd5721fa34ec69a
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-07-09 22:12:30 +0900

    feat: 거래 페이지 매물 구현

--- mission/script.js
+++ mission/script.js
@@ -1,1 +1,24 @@
 // 화이팅..
+// -> ㅠㅠ
+
+function list() {
+    fetch('data.json').then(function (response) {
+        return response.json();
+    }).then(function (obj) {
+        const container = document.querySelector('#imgcontainer');
+        let content = '';   
+
+        obj.data.map((item) => {
+            content += `
+            <div id="items">
+              <img src="${item.img}" alt="${item.name}">
+              <h2>${item.name}</h2>
+              <div> 999,999원 </div>
+              <p>${item.address}</p>
+            </div>
+          `;
+        });
+
+        container.innerHTML = content;
+    });
+}
