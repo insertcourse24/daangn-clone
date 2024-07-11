e55d9efd824e97433b4a384de460370e70707b7d
심현진 <hyunjin.dev143@gmail.com>
2024-07-11 09:30:35 +0900

    feat : 중고거래 페이지 기능 추가

--- mission/script.js
+++ mission/script.js
@@ -1,1 +1,12 @@
-// 화이팅..
+fetch('../data.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let html = data.data.map((item) => {
+            return `<img src="${item.img}" alt="${item.name}">
+            <span>${item.name}<span>
+            <p>${item.address}</p>`;
+        })
+        document.querySelector('.list').innerHTML = html;
+    });
